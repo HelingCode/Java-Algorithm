0aea132e66e9457c159854e718648964bb5681bc
HelingCode <929926540@qq.com>
2021-06-04 20:58:46 +0800

    Queue

---
+++ src/cn/cstube/dynamicarray/Main.java
@@ -0,0 +1,8 @@
+package cn.cstube.dynamicarray;
+
+/**
+ * @author Heling
+ * @date 2021/6/4
+ */
+public class Main {
+}
--- src/cn/cstube/queue/ArrayQueueDemo.java
+++ src/cn/cstube/queue/ArrayQueueDemo.java
@@ -1,11 +1,64 @@
 package cn.cstube.queue;
 
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
 /**
  * @author Heling
  * @date 2021/6/4
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
+        //测试
+        //创建一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        //接受用户的输入
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop){
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try{
+                        int res = queue.getQueue();
+                        System.out.println("取出的数据是："+res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try{
+                        int res = queue.headQueue();
+                        System.out.println("队列头的数据是：" + res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println("程序退出~~");
+
 
     }
 
